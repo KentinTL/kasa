e5d598c77a1db62acdd14208082dce4867250902
QuentinTL <q.thibaut.lazaro96@gmail.com>
2023-08-05 18:49:54 +0200

    Start Kasa
    Kasa project created and some features more

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class Home extends Component {
+    render() {
+        return (
+            <header>
+                <img src="../../../public/images/LOGO.png" alt="Logo Kasa" />
+                <ul>
+                    <li>Accueil</li>
+                    <li>A Propos</li>
+                </ul>
+            </header>
+        );
+    }
+}
+
+export default Home;
